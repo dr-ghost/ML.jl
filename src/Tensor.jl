49fa1e6a4cb2512d979f91df76fe02473bc1a4cc
Anshul Thakur <66345681+dr-ghost@users.noreply.github.com>
2022-10-29 12:00:41 +0000

    tensors working...

--- src/Tensor.jl
+++ src/Tensor.jl
@@ -5,7 +5,7 @@ module Tensor
 import Base.@pure
 
 import Statistics
-using Statistics : mean
+
 using LinearAlgebra
 using StaticArrays
 
@@ -13,34 +13,32 @@ include("autograd.jl")
 
 #exports
 export ⋅, ×, dot, diagm, tr, det, norm, eigvals, eigvecs, eigen, mean
-export AbstractTensor
+export AbstractTensor, tensor
 
 
 abstract type AbstractTensor{order, dim, T <: Number} <: AbstractArray{T, order} end
 
-struct Tensor{order, dim, T <: Number, M} <: AbstractTensor{order, dim, T}
+struct tensor{order, dim, T <: Number, M} <: AbstractTensor{order, dim, T}
     data::NTuple{M, T}
-    Tensor{order, dim, T, M}(data::NTuple) where {order, dim, T, M} = new{order, dim, T, M}(data)
+    tensor{order, dim, T, M}(data::NTuple) where {order, dim, T, M} = new{order, dim, T, M}(data)
 end
 
 # Utility functions
 
 get_data(t::AbstractTensor) = t.data
 
-@pure n_components(::Type{Tensor{order, dim}}) where {order, dim} = dim^order
+@pure n_components(::Type{tensor{order, dim}}) where {order, dim} = dim^order
 @pure get_type(::Type{Type{x}}) where {x} = x
-@pure get_base(::Type{<:Tensor{order, dim}}) where {order, dim} = Tensor{order, dim}
+@pure get_base(::Type{<:tensor{order, dim}}) where {order, dim} = Tensor{order, dim}
 
-@pure Base.eltype(::Type{Tensor{order, dim, T, M}}) where {order, dim, T, M} = T
-@pure Base.eltype(::Type{Tensor{order, dim, T}})    where {order, dim, T}    = T
-@pure Base.eltype(::Type{Tensor{order, dim}})       where {order, dim}       = Any
+@pure Base.eltype(::Type{tensor{order, dim, T, M}}) where {order, dim, T, M} = T
+@pure Base.eltype(::Type{tensor{order, dim, T}})    where {order, dim, T}    = T
+@pure Base.eltype(::Type{tensor{order, dim}})       where {order, dim}       = Any
 
 ##
-Base.IndexStyle(::Type{<:Tensor}) = IndexLinear()
+Base.IndexStyle(::Type{<:tensor}) = IndexLinear()
 
 ##size
-Base.size(t::Tensor{order, dim}) where {order, dim} = ntuple(i -> dim, order)
-Base.length(::Type{Tensor{order, dim, T, M}}) where {order, dim, T, M} = 
+@pure Base.size(::Type{tensor{order, dim}}) where {order, dim} = (dim, )^order
 
-@inline          Tensor{order, dim, T}(data::Union{AbstractArray, Tuple, Function}) where {order, dim, T} = convert(Tensor{order, dim, T}, Tensor{order, dim}(data))
-@inline          Tensor{order, dim, T, M}(data::Union{AbstractArray, Tuple, Function})  where {order, dim, T, M} = Tensor{order, dim, T}(data)
+end
